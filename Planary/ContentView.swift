cb84aa892fb7788464ddf972c53a1c6c724f5e4b
Pisit Jaiton <pisit.jaiton@mail.kmutt.ac.th>
2023-06-07 22:28:17 +0700

    init

---
+++ Planary/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Planary
+//
+//  Created by Pisit Jaiton on 7/6/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("สวัสดี").font(.custom("Inter-Regular", size: 24))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Planary/PlanaryApp.swift
@@ -0,0 +1,33 @@
+//
+//  PlanaryApp.swift
+//  Planary
+//
+//  Created by Pisit Jaiton on 7/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlanaryApp: App {
+    
+    init(){
+        printFonts()
+    }
+    
+    func printFonts() {
+        let fontFamilyNames = UIFont.familyNames
+        
+        for familyName in fontFamilyNames{
+            print("-------------")
+            print("Font Family name -> [\(familyName)]")
+            let names = UIFont.fontNames(forFamilyName: familyName)
+            print("Font name -> \(names)")
+        }
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
