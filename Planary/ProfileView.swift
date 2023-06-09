28e3c4335b0cc8cc875a7722b3a0bd0d3c78830a
Nattapat Sittichai <73817577+Voidntpx@users.noreply.github.com>
2023-06-09 22:25:11 +0700

    profile

--- Planary/ProfileView.swift
+++ Planary/ProfileView.swift
@@ -10,15 +10,45 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         ZStack {
-            Color.gray
+            Color(cgColor: CGColor(red: 0.094, green: 0.095, blue: 0.095, alpha: 1))
             VStack {
                 ZStack {
                     RoundedRectangle(cornerRadius: 30)
-                        .foregroundColor(Color.white)
+                        .foregroundColor(Color(CGColor(red: 0.167, green: 0.167, blue: 0.167, alpha: 1)))
                         .frame(width: .infinity, height: 160)
+                        .overlay(
+                                Text("Account Name")
+                                    .font(.system(size: 25,weight: .bold))
+                                    .foregroundColor(.white)
+                                    .padding()
+                            )
                     RoundedRectangle(cornerRadius: 360)
-                        .frame(width: 120, height: 120)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.gray)
                         .padding(.bottom, 150.0)
+                        .overlay(
+                                Image(systemName: "person.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 50, height: 50)
+                                    .padding(.bottom, 150.0)
+                                    .foregroundColor(Color.white)
+                            )
+                    RoundedRectangle(cornerRadius: 360)
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(Color(cgColor: CGColor(red: 0.094, green: 0.095, blue: 0.095, alpha: 1)))
+                        .padding(.bottom, 90.0)
+                        .padding(.leading, 70)
+                        .overlay(
+                                Image(systemName: "pencil")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 10)
+                                    .padding(.bottom, 90.0)
+                                    .padding(.leading, 70)
+                                    .foregroundColor(Color.white)
+                                    
+                            )
                     
                     
                 }.padding(.top, 50.0)
@@ -30,6 +60,7 @@ struct ProfileView: View {
                     Image(systemName: "greaterthan")
                         .padding(.trailing,25)
                 }.padding(.leading,20)
+                    .foregroundColor(Color.white)
                 Spacer()
                 HStack{
                     Image(systemName: "gear")
@@ -37,7 +68,7 @@ struct ProfileView: View {
                     Spacer()
                     Image(systemName: "greaterthan")
                         .padding(.trailing,25)
-                }.padding(.leading,20)
+                }.padding(.leading,20).foregroundColor(Color.white)
                 Spacer()
                 HStack{
                     Image(systemName: "gear")
@@ -45,7 +76,7 @@ struct ProfileView: View {
                     Spacer()
                     Image(systemName: "greaterthan")
                         .padding(.trailing,25)
-                }.padding(.leading,20)
+                }.padding(.leading,20).foregroundColor(Color.white)
                 Spacer()
                 HStack{
                     Image(systemName: "gear")
@@ -53,12 +84,12 @@ struct ProfileView: View {
                     Spacer()
                     Image(systemName: "greaterthan")
                         .padding(.trailing,25)
-                }.padding(.leading,20)
+                }.padding(.leading,20).foregroundColor(Color.white)
                 Spacer()
                 
             }
         }.edgesIgnoringSafeArea(.top)
-            .padding(.vertical)
+            .padding(.top)
             .overlay(
                 ZStack{
                     Color(CGColor(red: 0.167, green: 0.167, blue: 0.167, alpha: 1))
@@ -67,7 +98,7 @@ struct ProfileView: View {
                         .font(.system(size: 20).weight(.bold))
                         .foregroundColor(Color.white)
                         .padding(.top,60)
-                    Image(systemName: "bookmark")
+                    Image(systemName: "gearshape")
                         .frame(maxWidth: .infinity,alignment: .trailing)
                         .padding(.trailing,20)
                         .foregroundColor(Color.white)
@@ -77,11 +108,12 @@ struct ProfileView: View {
                 }
                     .frame(height: 110)
                     .frame(maxHeight: .infinity,alignment: .top)
-                    
+
                     .edgesIgnoringSafeArea(.top)
                     
                     
         )
+        
         
        
 
